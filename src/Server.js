bcd81a4692b125cc8e55e1ee7b013bcf3f7c77d7
santomi3112 <adiprasetio007@gmail.com>
2022-11-13 06:37:58 +0700

    initialized server using hapi

--- src/Server.js
+++ src/Server.js
@@ -1,3 +1,18 @@
 const Hapi = require('@hapi/hapi');
 
-console.log(Hapi);
+const init = async () => {
+  const server = Hapi.server({
+    port: 3001,
+    host: 'localhost',
+  });
+
+  await server.start();
+  console.log('server running on %s', server.info.uri);
+};
+
+process.on('unhandledRejection', (err) => {
+  console.log(err);
+  process.exit(1);
+});
+
+init();
